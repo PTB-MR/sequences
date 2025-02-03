b1289d3246cbe5b01a60a0f15f38d6da288b9b35
Patrick Schuenke <patrick.schuenke@gmail.com>
2025-02-03 09:46:02 +0100

    remove inversion time arg from t1prep

--- src/sequences/preparations/t1prep.py
+++ src/sequences/preparations/t1prep.py
@@ -8,12 +8,11 @@ from sequences.utils import sys_defaults
 def add_t1prep(
     seq: pp.Sequence | None = None,
     system: pp.Opts | None = None,
-    inversion_time: float = 21e-3,
     rf_duration: float = 10.24e-3,
     add_spoiler: bool = True,
     spoiler_ramp_time: float = 6e-4,
     spoiler_flat_time: float = 8.4e-3,
-) -> tuple[pp.Sequence, float]:
+) -> tuple[pp.Sequence, float, float]:
     """Add an adiabatic T1 preparation block to a sequence.
 
     The adiabatic inversion pulse is a hyperbolic secant pulse with default values similar to the one used by vendors.
@@ -30,7 +29,6 @@ def add_t1prep(
         Duration of the adiabatic inversion pulse (in seconds).
     add_spoiler
         Toggles addition of spoiler gradients after the inversion pulse.
-        The spoiler does not increase the total duration of the block, but limits the minimum inversion time.
     spoiler_ramp_time
         Duration of gradient spoiler ramps (in seconds).
     spoiler_flat_time
@@ -42,11 +40,8 @@ def add_t1prep(
         PyPulseq Sequence object.
     block_duration
         Total duration of the T1 preparation block (in seconds).
-
-    Raises
-    ------
-    ValueError
-        If the inversion time is too short for the given RF and spoiler durations.
+    time_to_midpoint
+        Time from start of the block to the midpoint of the inversion pulse (in seconds).
     """
     # set system to default if not provided
     if system is None:
@@ -57,14 +52,6 @@ def add_t1prep(
 
     # get current duration of sequence before adding T1 preparation block
     time_start = sum(seq.block_durations.values())
-
-    # calculate inversion time delay
-    total_spoil_time = (2 * spoiler_ramp_time + spoiler_flat_time) if add_spoiler else 0
-    tau = inversion_time - rf_duration / 2 - system.rf_ringdown_time - total_spoil_time
-
-    # check if delay is valid
-    if tau < 0:
-        raise ValueError('Inversion time too short for given RF and spoiler durations.')
 
     # Add adiabatic inversion pulse
     rf = pp.make_adiabatic_pulse(
@@ -90,10 +77,10 @@ def add_t1prep(
         )
         seq.add_block(gz_spoil)
 
-    if tau > 0:
-        seq.add_block(pp.make_delay(tau))
-
     # calculate total duration of T1prep block
     block_duration = sum(seq.block_durations.values()) - time_start
 
-    return (seq, block_duration)
+    # calculate time from start of the block to the midpoint of the inversion pulse
+    time_to_midpoint = system.rf_dead_time + rf_duration / 2
+
+    return (seq, block_duration, time_to_midpoint)
