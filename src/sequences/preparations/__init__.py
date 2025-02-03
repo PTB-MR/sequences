1d4bff97635d458164cce060fdc3b2148cf5d571
Patrick Schuenke <patrick.schuenke@gmail.com>
2025-02-03 07:39:15 +0100

    remove enforce __all__

--- src/sequences/preparations/__init__.py
+++ src/sequences/preparations/__init__.py
@@ -1,6 +1,3 @@
 from sequences.preparations.t1prep import add_t1prep
 from sequences.preparations.t2prep import add_composite_refocusing_block
 from sequences.preparations.t2prep import add_t2prep
-
-
-__all__ = ['add_composite_refocusing_block', 'add_t1prep', 'add_t2prep']
--- src/sequences/utils/__init__.py
+++ src/sequences/utils/__init__.py
@@ -1,3 +1,1 @@
 from sequences.utils.system_defaults import sys_defaults
-
-__all__ = ["sys_defaults"]
