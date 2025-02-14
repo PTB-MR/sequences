5e80c1b3581da14d02ecf1704e67c8906ae9f06b
Patrick Schuenke <patrick.schuenke@gmail.com>
2025-02-14 15:10:44 +0100

    code review: round_to_raster function

--- src/sequences/utils/sequence_helper.py
+++ src/sequences/utils/sequence_helper.py
@@ -1,10 +1,12 @@
 """Helper functions for the creation of sequences."""
 
+from typing import Literal
+
 import numpy as np
 
 
-def round_to_raster(value: float, raster_time: float) -> float:
-    """Round a value to the closest larger multiple of a raster time.
+def round_to_raster(value: float, raster_time: float, method: Literal['floor', 'round', 'ceil'] = 'round') -> float:
+    """Round a value to the given raster time using the defined method.
 
     Parameters
     ----------
@@ -12,10 +14,19 @@ def round_to_raster(value: float, raster_time: float) -> float:
         Value to be rounded.
     raster_time
         Raster time, e.g. gradient, rf or ADC raster time.
+    method
+        Rounding method. Options: "floor", "round", "ceil".
 
     Returns
     -------
     rounded_value
         Rounded value.
     """
-    return raster_time * np.ceil(value / raster_time)
+    if method == 'floor':
+        return raster_time * np.floor(value / raster_time)
+    elif method == 'round':
+        return raster_time * np.round(value / raster_time)
+    elif method == 'ceil':
+        return raster_time * np.ceil(value / raster_time)
+    else:
+        raise ValueError(f'Unknown rounding method: {method}. Expected: "floor", "round" or "ceil".')
